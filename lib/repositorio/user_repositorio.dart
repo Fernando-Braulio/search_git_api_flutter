81034ca5a1a7be527bf790475aa87b3055e1c344
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-11-06 23:10:13 -0300

    alguns comentarios

--- lib/repositorio/user_repositorio.dart
+++ lib/repositorio/user_repositorio.dart
@@ -2,11 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:searchgithub/model/user.dart';
 
+// FUNÇÃO PARA BUSCAR DADOS DA API, A FUNÇÃO RECEBE COMO PARAMETRO O username
+// É UMA FUNÇÃO Future POR QUE O RETORNO NÃO É INSTANTÂNEO, TEM QUE AGUARDAR O RETORNO, É UM MÉTODO ASSÍNCRONO
 Future<User> getUser(String username) async {
   final response = await http.get('https://api.github.com/users/${username}');
 
+  //CASO O RETORNO DA API SEJÁ CODIGO 200, CONSEGUIU TRAZER OS DADOS DO USUÁRIO
   if (response.statusCode == 200) {
-    return User.fromJson(jsonDecode(response.body));
+    return User.fromJson(jsonDecode(response.body)); // "CONVERTENDO" O RETORNO QUE É EM JSON PARA A CLASSE USER
   } else {
     throw ('Registro não encontrado!');
   }
